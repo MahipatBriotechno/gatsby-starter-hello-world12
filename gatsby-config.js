6c7c1c88a6e890d675e40a4edc974f9291434276
Mahipat.Briotechno <102782100+MahipatBriotechno@users.noreply.github.com>
2022-07-01 23:43:02 +0530

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Blog App`,
+    siteStories: {
+      title: `Stories of Our Time`,
+    },
+    siteEvents: {
+      title: `Pandas Eating Lots`,
+    },
+    siteAbout: {
+      title: `About CSS Modules`,
+    },
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-airtable',
+      options: {
+        apiKey: 'keyT4kaMtXoFjjGqh',
+        baseId: 'app7Dd4JJ1IwhOgHS',
+        tableName: 'cms',
+        tableView: 'published',
+        queryName: ''
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    {
+    resolve: `gatsby-plugin-typography`,
+    options: {pathToConfigModule: `src/utils/typography.js`},
+    },
+  ],
 }
